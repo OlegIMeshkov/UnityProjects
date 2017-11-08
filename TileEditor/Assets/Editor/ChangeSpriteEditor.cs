c161acd8796f683d63212000dc7fd0e2379905a9
Oleg Meshkov <afdrills@gmail.com>
2017-11-08 09:31:50 +0300

    TileEditor Proj Added
    3 functions released
    1. Style Change
    2. Snap to the grid with controllable grid step
    3. Customizable sorting order through the script

---
+++ TileEditor/Assets/Editor/ChangeSpriteEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(ChangeSprite))]
+public class ChangeSpriteEditor : Editor {
+	
+
+
+	public override void OnInspectorGUI ()
+	{
+		base.OnInspectorGUI ();	
+
+
+
+		GUILayout.BeginHorizontal ();
+
+		if (GUILayout.Button ("ToStone")) 
+		{
+			ChangeSprite changeS = (ChangeSprite)target;
+			changeS.ChangeSpriteToStone ();
+		}
+		if (GUILayout.Button ("ToBase")) 
+		{
+			ChangeSprite changeS = (ChangeSprite)target;
+			changeS.ChangeSpriteToBase ();
+		}
+		if (GUILayout.Button ("ToWooden")) 
+		{
+			ChangeSprite changeS = (ChangeSprite)target;
+			changeS.ChangeSpriteToWooden ();
+		}
+
+		GUILayout.EndHorizontal ();
+
+
+	}
+
+	void OnGUI()
+	{
+		ChangeSprite spr = (ChangeSprite)target;
+		foreach (GameObject obj in Selection.gameObjects) {
+			obj.transform.position = new Vector3 (Mathf.Round (obj.transform.position.x), Mathf.Round (obj.transform.position.y), Mathf.Round (obj.transform.position.z)) * spr.grid;
+
+		}
+	}
+}
---
+++ TileEditor/Assets/Resources/Scripts/ChangeSprite.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ChangeSprite : MonoBehaviour {
+	public float grid = 0.5f;
+
+
+
+	void Start()
+	{
+		
+
+	}
+
+	void Update()
+	{
+		SetRoundPos ();
+		SetSortingLayer ();
+
+	}
+
+	public void ChangeSpriteToStone()
+	{
+		
+		foreach (Transform child in gameObject.transform)
+		{
+			SpriteRenderer spr = child.gameObject.GetComponent<SpriteRenderer> ();
+			if (spr) 
+			{
+				
+				string imageFilePath = "Sprites/stone/"+spr.sprite.name;
+				Debug.Log (imageFilePath);
+				spr.sprite = Resources.Load (imageFilePath, typeof(Sprite)) as Sprite;
+			}
+		}
+	}
+
+	public void ChangeSpriteToBase()
+	{
+
+		foreach (Transform child in gameObject.transform)
+		{
+			SpriteRenderer spr = child.gameObject.GetComponent<SpriteRenderer> ();
+			if (spr) 
+			{
+
+				string imageFilePath = "Sprites/base/"+spr.sprite.name;
+				Debug.Log (imageFilePath);
+				spr.sprite = Resources.Load (imageFilePath, typeof(Sprite)) as Sprite;
+			}
+		}
+	}
+
+	public void ChangeSpriteToWooden()
+	{
+
+		foreach (Transform child in gameObject.transform)
+		{
+			SpriteRenderer spr = child.gameObject.GetComponent<SpriteRenderer> ();
+			if (spr) 
+			{
+
+				string imageFilePath = "Sprites/wooden/"+spr.sprite.name;
+				Debug.Log (imageFilePath);
+				spr.sprite = Resources.Load (imageFilePath, typeof(Sprite)) as Sprite;
+			}
+		}
+	}
+
+	void SetSortingLayer()
+	{
+		foreach (Transform child in gameObject.transform)
+		{
+			SpriteRenderer spr = child.gameObject.GetComponent<SpriteRenderer> ();
+			if (spr.sprite.name.Contains ("wall")) {
+				spr.sortingOrder = 1;
+			} else if (spr.sprite.name.Contains ("floor")) {
+				spr.sortingOrder = 0;
+			} else
+				return;
+			}
+	}
+
+
+	void SetRoundPos()
+	{
+		float recGrid = 1 / grid;
+		foreach (Transform child in gameObject.transform)
+		{
+			child.transform.localPosition = new Vector3 (Mathf.Round (child.transform.localPosition.x* recGrid)/recGrid, Mathf.Round (child.transform.localPosition.y * recGrid)/recGrid, Mathf.Round (child.transform.localPosition.z));
+		}
+	}
+
+
+}
